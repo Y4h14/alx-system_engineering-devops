dc7508061e0114b92938b0164f7cd882b9bff126
Y4h14 <yahyasadig2013@gmail.com>
2024-02-24 20:22:17 +0300

    task 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# modifying client's ssh config file
+
+file_line {'diable pass auth'
+	ensure  => present,
+	path    => '/etc/ssh/ssh_config',
+	line    => '	PasswordAuthentication no',
+	replace => true,
+}
+
+file_line { 'identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
