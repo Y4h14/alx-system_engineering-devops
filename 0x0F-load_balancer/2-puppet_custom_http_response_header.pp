750c21d22862aaa158aee378e4880359cecf93bb
Y4h14 <yahyasadig2013@gmail.com>
2024-03-05 23:12:45 +0300

    \task2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,33 @@
+# automating creating a custom HTTP header
+
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "server {
+        listen  80 default_server;
+        listen  [::]:80 default_server;
+        root    /var/www/html;
+        index   index.html;
+
+        # Add custom header
+        add_header X-Served-By \$hostname;
+
+        location /redirect_me {
+        return 301 http://googl.com;
+        }
+        error_page 404 /404.html;
+        location /404 {
+        root /var/www/html;
+        internal;",
+  notify  => Service['nginx'],
+    
+service { 'nginx':
+  ensure     => running,
+}
