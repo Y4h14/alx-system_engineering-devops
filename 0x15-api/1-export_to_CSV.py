f19a93f3a0557acd7940125971b8e1686d3aa7d0
Y4h14 <yahyasadig2013@gmail.com>
2024-03-26 04:53:40 +0300

    updated task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,12 +14,10 @@ if __name__ == '__main__':
     user_req = requests.get(user_url).json()
     todo_req = requests.get(todo_url).json()
 
-    done_tasks = sum(1 for task in todo_req if task['completed'] is True)
-    total_tasks = sum(1 for task in todo_req)
     file_name = "{}.csv".format(user_id)
 
     with open(file_name, 'w', newline='') as f:
-        writer = csv.writer(f)
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         writer.writerow(["USER_ID", "USERNAME",
                          "TASK_COMPLETED_STATUS", "TASK_TITLE"])
         [writer.writerow([user_id, user_req['username'],
