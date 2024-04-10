07e70725d8bfc53ca6696669ff103131cd1c718e
Y4h14 <yahyasadig2013@gmail.com>
2024-04-10 18:12:03 +0300

    added user agent to task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     """gets the total number of subs from reddit's API"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent':'0x16-api_advanced:project:v1.0.0'}
 
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
     if response.status_code == 404:
         return 0
     data = response.json().get('data')
