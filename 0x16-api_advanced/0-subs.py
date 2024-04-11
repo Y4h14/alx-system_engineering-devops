82f4473680eee001312200af1da86c2b92249297
Y4h14 <yahyasadig2013@gmail.com>
2024-04-11 09:24:54 +0300

    returned to orginal implementaion on task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,13 +5,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """gets the total number of subs from reddit's API"""
-    if not subreddit or type(subreddit) is not str:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    response = requests.get(url)
+    if response.status_code == 404:
         return 0
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        r = r.json()
-    else:
-        return 0
-    return r.get('data', {}).get('subscribers', 0)
+    data = response.json().get('data')
+    return data.get("subscribers")
